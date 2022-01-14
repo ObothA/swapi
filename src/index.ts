f7795b78401edc74bca59eb98bd375926ff618c0
obotha <oboth.abel.oa@gmail.com>
2022-01-14 16:02:07 +0300

    Add inverse color scheme to index log.

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import app from './app';
 
 const PORT = process.env.PORT ?? 5000;
 const server = app.listen(PORT, () => {
-  console.log(colors.yellow.bold(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+  console.log(colors.inverse(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 });
 
 // Catch unhandled promise rejections
