4b4119ad134fcb6ebbd1e0d6ff97ed5b27939c20
obotha <oboth.abel.oa@gmail.com>
2022-01-15 15:10:55 +0300

    Return movie titles, id, opening crawl and release dates.

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -1,22 +1,38 @@
 import { RequestHandler } from 'express';
 import axios from 'axios';
 
+import ErrorResponse from '../utils/errorResponse';
+
 type Movie = {
   title: string;
+  string: string;
+  release_date: string;
+  opening_crawl: string;
 };
 
 type Film = {
-  properties: { title: string };
+  properties: {
+    title: string;
+    release_date: string;
+    opening_crawl: string;
+  };
+  uid: string;
 };
 
-export const getMovies: RequestHandler = async (req, res) => {
-  const movieREsponse = await axios.get(`${process.env.SWAPI_URL}/films`);
-  console.log(movieREsponse.data.result);
+export const getMovies: RequestHandler = async (req, res, next) => {
+  const movieResponse = await axios.get(`${process?.env?.SWAPI_URL}/films`);
+  const rawMovies = movieResponse?.data?.result;
 
-  const rawMovies = movieREsponse.data.result;
-  const movies: Movie[] = rawMovies.map((film: Film) => ({
-    title: film?.properties?.title,
-  }));
+  if (rawMovies) {
+    const movies: Movie[] = rawMovies?.map((film: Film) => ({
+      id: film?.uid,
+      title: film?.properties?.title,
+      opening_crawl: film?.properties?.opening_crawl,
+      release_date: film?.properties?.release_date,
+    }));
 
-  res.send(movies);
+    res.send(movies);
+  } else {
+    return next(new ErrorResponse('No movies were returned from the SWAPI API'));
+  }
 };
