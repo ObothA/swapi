44430d3f7bd03309276c9d8b351175b25dccfa3f
obotha <oboth.abel.oa@gmail.com>
2022-01-15 11:22:52 +0300

    Add axios

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -1,5 +1,8 @@
 import { RequestHandler } from 'express';
+import axios from 'axios';
 
 export const getMovies: RequestHandler = async (req, res) => {
+  const movieREsponse = await axios.get(`${process.env.SWAPI_URL}/films`);
+  console.log(movieREsponse.data);
   res.send('Hello World!');
 };
