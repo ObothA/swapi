31a7f81b694c047b1732f80467cf0c6a6d9277f3
obotha <oboth.abel.oa@gmail.com>
2022-01-15 11:33:03 +0300

    Return titles of star wars movies from swapi.

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -1,8 +1,22 @@
 import { RequestHandler } from 'express';
 import axios from 'axios';
 
+type Movie = {
+  title: string;
+};
+
+type Film = {
+  properties: { title: string };
+};
+
 export const getMovies: RequestHandler = async (req, res) => {
   const movieREsponse = await axios.get(`${process.env.SWAPI_URL}/films`);
-  console.log(movieREsponse.data);
-  res.send('Hello World!');
+  console.log(movieREsponse.data.result);
+
+  const rawMovies = movieREsponse.data.result;
+  const movies: Movie[] = rawMovies.map((film: Film) => ({
+    title: film?.properties?.title,
+  }));
+
+  res.send(movies);
 };
