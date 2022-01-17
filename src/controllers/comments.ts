8826513ef0b15a870bdba16b3749d57412f80159
obotha <oboth.abel.oa@gmail.com>
2022-01-17 14:36:51 +0300

    Add a comment to the planet scale database.

--- src/controllers/comments.ts
+++ src/controllers/comments.ts
@@ -1,3 +1,20 @@
 import { RequestHandler } from 'express';
 
-export const addComment: RequestHandler = async (req, res) => {};
+import prisma from '../config/client';
+
+export const addComment: RequestHandler = async (req, res) => {
+  const { movieId, comment } = req.body;
+
+  const newComment = await prisma.comments.create({
+    data: {
+      movie_id: movieId,
+      comment,
+    },
+  });
+
+  res.send({
+    success: true,
+    message: 'Comment added successfully.',
+    data: newComment,
+  });
+};
--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -1,10 +1,31 @@
 import { Router } from 'express';
+import { check, CustomSanitizer } from 'express-validator';
 
 import asyncHandler from '../middleware/async';
+import ErrorResponse from '../utils/errorResponse';
+import expressValidatorHandler from '../middleware/expressValidatorHandler';
 import { addComment } from '../controllers/comments';
 
 const router = Router();
 
-router.post('/', asyncHandler(addComment));
+const sanitizeMovieId: CustomSanitizer = (value: string) => {
+  if (typeof value === 'string') {
+    return value;
+  }
+
+  throw new ErrorResponse('movieId should be a string', 422);
+};
+
+router.post(
+  '/',
+  check('movieId')
+    .notEmpty()
+    .withMessage('Please provide a movie i.d')
+    .bail()
+    .customSanitizer(sanitizeMovieId),
+  check('comment').notEmpty().withMessage('Please provide a comment'),
+  expressValidatorHandler,
+  asyncHandler(addComment)
+);
 
 export default router;
